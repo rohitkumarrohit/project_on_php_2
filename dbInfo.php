c510d1b80166b0156478a8c929072725a21ef915
rohitkumarrohit <36002304+rohitkumarrohit@users.noreply.github.com>
2018-03-29 16:23:55 +0530

    Add files via upload

---
+++ dbInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbPassword = "phpfundamentals";
+$dbUserName = "phpfundamentals";
+$dbServer = "localhost";
+$dbName = "phpfundamentals";
+
+$connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName);
+
+if($connection->connect_errno)
+{
+    exit("Database Connection Failed. Reason: ".$connection->connect_error);
+}
+
+$query = "SELECT id, first_name, last_name, pen_name FROM Authors ORDER BY first_name";
+$resultObj = $connection->query($query);
+
+?>
